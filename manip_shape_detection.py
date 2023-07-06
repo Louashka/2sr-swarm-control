3754df41abc695852bbaed9490621aa994a779e5
AdViVai <vishanov.ad@yandex.ru>
2023-07-06 07:39:17 +0300

    get contours with OpenCV

--- manip_shape_detection.py
+++ manip_shape_detection.py
@@ -1,1 +1,25 @@
 import cv2 as cv
+import numpy as np
+cap = cv.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    blurred = cv.GaussianBlur(gray, (9,9), 0)
+    edges = cv.Canny(blurred, 50, 100)
+    _, contour = cv.threshold(edges, 140, 255, cv.THRESH_BINARY)
+
+    contours, _ = cv.findContours(contour, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+    cv.imshow('Contour Detection', frame)
+
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv.destroyAllWindows()
